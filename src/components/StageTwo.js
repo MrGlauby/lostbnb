4d307b12db0ea3f9a728a50ec1bd1ebd4434cfa9
Christoph Nientied <christophnientied@googlemail.com>
2023-04-27 18:09:48 +0200

    StageTwo first version done

---
+++ src/components/StageTwo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+//import './styles/StageTwo.css';
+import img1 from '../img/stageTwoContainerLeftImg1.jpg';
+import img2 from '../img/stageTwoContainerLeftImg2.jpg';
+
+
+
+export default function StageTwo() {
+  return (
+    <>
+    <div className ="stageTwoContainer">
+    <div className ="stageTwoContainerLeft">
+    <img className ="stageTwoContainerLeftImg1" src={img1} alt="Lost Place in Piemont, Italy"/>    
+    <img className ="stageTwoContainerLeftImg2" src={img2} alt="Lost Place in Berlin, Germany"/>
+    </div>
+    <div className="stageTwoContainerRight">
+        <h3>About us</h3>
+        <br/>
+        <h1>Discover the forgotten, explore the unknown - uncover the mysteries of lost places!
+</h1>
+        <br/>
+        <p>Are you an adventurer at heart? Do you have a fascination for the forgotten and abandoned? Then join us on an unforgettable journey to explore the world's lost places.Let us take you on a journey of discovery, where you can witness firsthand the beauty and mystery of these forgotten places. Book your tour today and be ready for an experience like no other!</p>
+        <br/>
+    <div className="stageTwoContainerRightSpanContainer"> 
+        <p><b>15</b> <br/>Years of <br/>Experience</p>
+        <p><b>1k</b> <br/>Sucessful <br/>Trips</p>
+        <p><b>20k</b> <br/>Happy <br/>Customers</p>
+        <p><b>4.9</b> <br/>Overall <br/>Rating</p>
+    </div> 
+    </div>
+  </div>
+  </>
+  )
+}
