0643438fc24947ddc9083065c897d7167b4c2acd
Thomas <test@gmail.com>
2023-05-02 16:26:55 +0200

    stageone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import StageOne from './components/StageOne.js';
+import './styles/StageOne.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+
+
       </header>
+
+      <StageOne />
+
     </div>
   );
 }
---
+++ src/components/StageOne.js
@@ -0,0 +1,46 @@
+import React from "react";
+import img2 from "../img/StageOne_2.jpg";
+
+export default function StageOne() {
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="left">
+          <h2 className="dareto">
+            Dare to live the life you've always wanted.
+          </h2>
+          <p className="smaltext">
+            Life is short and the world is wide. So, better get started.
+          </p>
+
+          <div className="whenwehre">
+            <div className="search">
+              <label for="location">Location</label>
+              <select name="land" id="land">
+                <option empty="empty"> Wo willste hin?</option>
+                <option woanders="woanders">Daheim</option>
+                <option woanders="woanders">Nicht Daheim</option>
+              </select>
+            </div>
+
+            <div className="date">
+              <label for="date">Date</label>
+              <select name="day" id="day">
+                <option empty="empty"> Wann?</option>
+                <option woanders="woanders">Datum...</option>
+                <option woanders="woanders">Datum...</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+       
+          <div className="bigpic">
+            <img className="bild2" src={img2} alt="bild1" />
+          </div>
+        
+      </div>
+    </div>
+  );
+}
+
