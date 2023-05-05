1eb28efd115eaf48b92c0effb21019a69a4ecb37
Renja <renjaschmakeit@gmail.com>
2023-05-05 17:25:50 +0200

    WIP Tried to fix object creation in places Section.

--- src/components/PlacesSection.js
+++ src/components/PlacesSection.js
@@ -7,30 +7,33 @@ export default function PlacesSection() {
   const [fetchedPlaces, setFetchedPlaces] = useState([])
   const [places, setPlaces] = useState([])
   function createPlaceObj(data) {
+    // const reviews = data.fields.reviews.map()
+    console.log("create Places data: ", data);
     const placesObject = 
         {
-          entryID: data.sys.id,
-          entryCreatedAt: data.sys.createdAt,
-          entryName: data.fields.name,
-          entryPic: data.fields.mainPicture.id,
-          entryDescription: data.fields.description.content.content.value,
-          entryRating: data.fields.rating,
-          entryGallery: data.fields.galleryPictures.sys.id,
-          entryLocation: data.fields.location.id,
-          entryReviews: data.fields.reviews.sys.id
+          id: data.sys.id,
+          createdAt: data.sys.createdAt,
+          name: data.fields.name,
+          pic: data.fields.mainPicture.sys.id,
+          description: data.fields.description.content[0].content[0].value,
+          rating: data.fields.rating,
+          // entryGallery: data.fields.galleryPictures.sys.id,
+          location: data.fields.location.sys.id,
+          // entryReviews: data.fields.reviews.sys.id
         }
-        setPlaces((prev) => {return [...prev, placesObject]})
+        setPlaces((prev) => {[...prev, placesObject]});
+        if (places !== []){
+        console.log("New Places Function:", places)
+        }
     }
   
-
   useEffect(() => {
     fetchContentTypeAll('places')
     .then((response) => {
-      setFetchedPlaces(response);
-    })
-    .then(()=> {
+      setFetchedPlaces(()=> {return response});
       console.log("fetchedPlaces: ", fetchedPlaces);
-      fetchedPlaces.map((place) => {
+      response.items.map((place) => {
+        console.log("map Place: ", place);
         createPlaceObj(place);
       })
     })
