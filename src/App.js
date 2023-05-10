409e057f69ce583ace3e8fb23dad27237935fb91
Christoph Nientied <christophnientied@googlemail.com>
2023-05-10 12:06:33 +0200

    finalized booking calender

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
-
 import './styles/StageTwo.css'
-
 import './styles/StageOne.css';
-
 import './styles/TravellersSection.css';
+
+import { Routes, Route, NavLink } from "react-router-dom";
 
 import StageOne from './components/StageOne.js';
 import StageTwo from './components/StageTwo';
 import TravellersSection from './components/TravellersSection';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-
 import PlaceGallery from './components/PlaceGallery.js';
 
 function App() {
   return (
     <div className="App">
-
     <Header />
     <PlaceGallery />
     <StageOne />
--- src/components/BookingCalender.js
+++ src/components/BookingCalender.js
@@ -1,24 +1,17 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Calendar from "react-calendar";
+import { useParams } from "react-router-dom";
 
-import { fetchContentTypeAll } from "../utils/contentfulApi";
-
-export default function BookingCalender() {
-  const [placeName, setPlaceName] = useState("");
+export default function BookingCalender( {}) {
   const [date, setDate] = useState(new Date());
+  const {  } = useParams();
 
-  useEffect(() => {
-    fetchContentTypeAll("places").then((response) => {
-      console.log("placefetch:", response);
-      setPlaceName(response.items[0].fields.name);
-    });
-  }, []);
 
   return (
     <>
-      <h1 className="bookingCalender-header">
-        Choose when and how long you want to be lost in the {placeName}
+    <h1 className="bookingCalender-header">
+        Choose when and how long you want to be lost:
       </h1>
       <br />
       <br />
